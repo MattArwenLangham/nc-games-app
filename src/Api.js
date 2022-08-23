1c7f393cbcc2f2ec09607da8358fde5b8240d4a7
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-08-23 11:58:52 +0100

    complete ticket 5, get reviews by category for mobile and desktop

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,15 @@
 import axios from 'axios'
 
-export const fetchReviews = () => {
-    return axios.get('https://matts-nc-games.herokuapp.com/API/reviews')
+export const fetchReviews = (category) => {
+    let searchParams = ""
+
+    if(category) searchParams = "?category=" + category
+
+    return axios.get(`https://matts-nc-games.herokuapp.com/API/reviews${searchParams}`)
     .then(({data : reviews}) => reviews)
+}
+
+export const fetchCategories = () => {
+    return axios.get('https://matts-nc-games.herokuapp.com/API/categories')
+    .then(({data : categories}) => categories)
 }
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,24 @@
+import '../stylesheets/categories.css'
+import Category from './Category'
+import { fetchCategories } from '../Api'
+import { useState, useEffect } from 'react' 
+
 const Categories = () => {
-    return <h1>Categories</h1>
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetchCategories()
+        .then(({categories: categoriesArray}) => {
+            setCategories(categoriesArray)
+        })
+    }, [])
+
+    return <div className="categories-container">
+        {categories.map((category) => {
+            return <Category info={category} key={category.category_id}/>
+        })}
+    </div>
 }
 
 export default Categories
